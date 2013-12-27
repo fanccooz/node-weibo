1487f0ac9f3dd0e1ca621c3d586ad978da943cf9
fanccooz <fangfang.ouyang@gmail.com>
2013-12-27 17:27:32 +0800

    more

--- app.js
+++ app.js
@@ -8,20 +8,27 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var MongoStore = require('connect-mongo')(express); 
+var settings = require('./settings');
 
-var app = express();
+var app = module.exports = express.createServer();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.configure(function(){ app.set('views', __dirname + '/views'); 
+    app.set('view engine', 'ejs'); 
+    app.use(express.bodyParser()); 
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: settings.cookieSecret,
+        store: new MongoStore({
+            db: settings.db
+        })
+     }));
+    app.use(express.router(routes));
+    app.use(express.static(__dirname + '/public'));
+});
 
 // development only
 if ('development' == app.get('env')) {
@@ -31,6 +38,5 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(3000);
+
